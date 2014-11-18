e2254b13e7faa0fb99cecd02f42b15dc53f57009
John Krah <john.krah@socrata.com>
2014-11-17 23:21:58 -0800

    choosing data types and functions

--- chapter07/parallel.scala
+++ chapter07/parallel.scala
@@ -1,3 +1,14 @@
 // end goal: map function to list in parallel
- val outputList = parMap(inputList)(f)
+// val outputList = parMap(inputList)(f)
+
+def sum(ints: Seq[Int]): Int = 
+	ints.foldLeft(0)((a,b) => a+b)
+
+def sum(ints: IndexedSeq[Int]): Int =
+	if (ints.size <= 1)
+		ints.headOption getOrElse 0
+	else {
+		val (l,r) = ints.splitAt(ints.length/2)
+		sum(l) + sum(r)
+	}
 
