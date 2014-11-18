8cf1543d68dac4878de8cc2cd8320c39ab12af0d
John Krah <john.krah@socrata.com>
2014-11-17 23:40:49 -0800

    combining parallel computations

--- chapter07/parallel.scala
+++ chapter07/parallel.scala
@@ -13,3 +13,10 @@ def sum(ints: IndexedSeq[Int]): Int =
 		val sumR: Par[Int] = Par.unit(sum(r))
 		Par.get(sumL) + Par.get(sumR)
 	}
+
+trait Par[+A]
+object Par {
+	def unit[A](a: => A): Par[A] = ???
+	def get[A](a: Par[A]): A = ???
+	def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
+}
