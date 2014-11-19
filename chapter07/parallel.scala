393053efc9fab936d5a3370b6f222af88193b51d
John Krah <john.krah@socrata.com>
2014-11-19 00:42:35 -0800

    explicit forking

--- chapter07/parallel.scala
+++ chapter07/parallel.scala
@@ -4,19 +4,21 @@
 def sum(ints: Seq[Int]): Int = 
 	ints.foldLeft(0)((a,b) => a+b)
 
-def sum(ints: IndexedSeq[Int]): Int =
+def sum(ints: IndexedSeq[Int]): Par[Int] =
 	if (ints.size <= 1)
-		ints.headOption getOrElse 0
+		Par.unit(ints.headOption getOrElse 0)
 	else {
 		val (l,r) = ints.splitAt(ints.length/2)
-		val sumL: Par[Int] = Par.unit(sum(l))
-		val sumR: Par[Int] = Par.unit(sum(r))
-		Par.get(sumL) + Par.get(sumR)
+		Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_+_)
 	}
 
 trait Par[+A]
 object Par {
 	def unit[A](a: => A): Par[A] = ???
-	def get[A](a: Par[A]): A = ???
+	def run[A](a: Par[A]): A = ???
+	def fork[A](a: => Par[A]): Par[A] = ???
+	def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 	def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
 }
+
+Par.map2(Par.unit(1), Par.unit(1))(_+_)
